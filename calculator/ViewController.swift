c7a8f5074fc14650a10db6367e5886fa28ecb85c
GunleoN <l.vito@163.com>
2015-10-01 14:35:59 +0800

    Add a new text file

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var text1: UITextField!
+    @IBOutlet weak var text2: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
 
+    @IBAction func calculate() {
+        var num1 = self.text1.text.toInt()
+        var num2 = self.text2.text.toInt()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
